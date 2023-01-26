7960a0f195a988942610c9266173fa94e5fe6788
Sarraakhalil <kw6.sasm@gmail.com>
2023-01-26 12:25:35 +0300

    new

---
+++ MovieProject/Movie-Project/Movie_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ProjectApp.swift
+//  Movie-Project
+//
+//  Created by سرّاء. on 04/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
